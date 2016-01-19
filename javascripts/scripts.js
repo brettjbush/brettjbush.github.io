213dfb7fab2b90a22f07fc2612a30b2e90de967a
Brett Bush <brettjbush@gmail.com>
2016-01-18 20:48:04 -0600

    master make nav more versatile between mobile and desktop

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,13 +1,43 @@
-function hamburgerToggle()
+// media query event handler
+if (matchMedia) {
+	var mq = window.matchMedia("(min-width: 768px)");
+	mq.addListener(WidthChange);
+	WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+
+	if (mq.matches) {
+		// window width is at least 768px
+    var elements = document.getElementsByClassName("dropdown-toggle");
+    for (var i = 0, len = elements.length; i < len; i++) {
+        addClass(elements[i], "disabled");
+    }
+	}
+	else {
+		// window width is less than 768px
+    // window width is at least 768px
+    var elements = document.getElementsByClassName("dropdown-toggle disabled");
+    for (var i = 0, len = elements.length; i < len; i++) {
+        removeClass(elements[i], "disabled");
+    }
+	}
+
+}
+
+function hasClass(el, name) {
+   return new RegExp('(\\s|^)'+name+'(\\s|$)').test(el.className);
+}
+
+function addClass(el, name)
 {
-  var hamburgerIcon = document.getElementById('hamburger-dropdown');
+   if (!hasClass(el, name)) { el.className += (el.className ? ' ' : '') +name; }
+}
 
-  if( hamburgerIcon.display == block )
-  {
-    hamburgerIcon.display = none;
-  }
-  else {
-    hamburgerIcon.display = block;
-  }
-
+function removeClass(el, name)
+{
+   if (hasClass(el, name)) {
+      el.className=el.className.replace(new RegExp('(\\s|^)'+name+'(\\s|$)'),' ').replace(/^\s+|\s+$/g, '');
+   }
 }
