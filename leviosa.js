6246293e9a4ae43abf1dd7363958e3dbe9228447
Brett Bush <brettjbush@gmail.com>
2018-01-05 13:30:10 -0600

    make fade limit 180

--- leviosa.js
+++ leviosa.js
@@ -33,7 +33,7 @@ function calcOffset(depth, angle) {
 }
 
 function calcOpacity() {
-  return 1 - (Math.sqrt(Math.abs(currentGamma * currentGamma) + Math.abs(currentBeta * currentBeta)) / 90);
+  return 1 - (Math.sqrt(Math.abs(currentGamma * currentGamma) + Math.abs(currentBeta * currentBeta)) / 180);
 }
 
 // Triggered on device tilt
