e5cb8ebe3e12d9fff1a43967b3a39bdf0f6b1265
Brett Bush <brettjbush@gmail.com>
2018-01-04 19:22:34 -0600

    Update documentation and cleanup leviosa.js

--- leviosa.js
+++ leviosa.js
@@ -1,17 +1,31 @@
-var shadowBoxes = document.getElementsByClassName("shadow-box");
+// Obtain leviosa boxes
+var leviosaBoxes = document.getElementsByClassName("leviosa");
+
+// Various depth constants for shadows (distance to background in pixels)
 var depthConstant = 40;
 var depthConstant2 = 80;
+
+// The current vertical and horizontal tilts
 var currentBeta = 0;
 var currentGamma = 0;
 
+// Difference from start angle to "up"
 var initDiffFromUpBeta = 0;
 var initDiffFromUpGamma = 0;
 
+// Marker to check first
 var first = true;
-var degtorad = Math.PI / 180; // Degree-to-Radian conversion
 
+// Convert multiply to degrees to get radians
+var degtorad = Math.PI / 180;
+
+// Calculate the shadow offset from angle
 function calcOffset(depth, angle) {
+  
+  // Tangent * Adjacent = Opposite
   var offsetValue = Math.tan(Math.abs(angle) * degtorad) * depth;
+  
+  // Account for correct shadow direction
   if(angle < 0)
   {
     offsetValue = offsetValue * -1;
@@ -19,7 +33,10 @@ function calcOffset(depth, angle) {
   return offsetValue;
 }
 
+// Triggered on device tilt
 window.addEventListener("deviceorientation", function(event) {
+  
+  // Initialize where "up" is
   if(first)
   {
     originalBeta = Number(event.beta).toFixed(1);
@@ -30,18 +47,24 @@ window.addEventListener("deviceorientation", function(event) {
 
     first = false;
   }
+  
+  // Vertical tilt
   if(event.beta != null)
   {
     var deltaBetaValue = event.beta - originalBeta;
     currentBeta = parseInt(Number(calcOffset(depthConstant, deltaBetaValue + initDiffFromUpBeta)).toFixed(0));
   }
+  
+  // Horizontal tilt
   if(event.gamma != null)
   {
     var deltaGammaValue = event.gamma - originalGamma;
     currentGamma = parseInt(Number(calcOffset(depthConstant, deltaGammaValue + initDiffFromUpGamma)).toFixed(0));
   }
-    for(i = 0; i < shadowBoxes.length; i++)
-    {
-      shadowBoxes[i].style.boxShadow = currentGamma.toString() + "px " + currentBeta.toString() + "px 20px #888888";
-    }
+  
+  // Apply change to all leviosa boxes
+  for(i = 0; i < leviosaBoxes.length; i++)
+  {
+    leviosaBoxes[i].style.boxShadow = currentGamma.toString() + "px " + currentBeta.toString() + "px 20px #888888";
+  }
 }, true);
